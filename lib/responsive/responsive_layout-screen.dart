53f4090a8fc388e1f5bcf28f263416c7d358950f
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-01 15:53:18 +0100

    revamp code and build layout

---
+++ lib/responsive/responsive_layout-screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:instagram_flutter/utils/dimension.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout(
+      {super.key,
+      required this.webScreenLayout,
+      required this.mobileScreenLayout});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxHeight > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
