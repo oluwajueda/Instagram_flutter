6882bbe91ade42e492d5e5ccccb13a22e9c3c2a2
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-04 19:25:22 +0100

    create pages for bottom navigation

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -14,10 +14,19 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  late PageController pageController;
   @override
   void initState() {
     super.initState();
     // getUsername();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    pageController.dispose();
   }
 
   // void getUsername() async {
@@ -31,56 +40,80 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   //   });
   // }
 
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
   int _page = 0;
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // print(user.username);
     return Scaffold(
-      body: Center(child: Text("This is mobile ")),
-      bottomNavigationBar:
-          BottomNavigationBar(backgroundColor: mobileBackgroundColor, items: [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
-            color: _page == 0 ? Colors.black54 : secondaryColor,
+      body: PageView(
+        children: [
+          Text("feed"),
+          Text("search"),
+          Text("add post"),
+          Text("notif"),
+          Text("profile"),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? Colors.black54 : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
           ),
-          label: "",
-          backgroundColor: primaryColor,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.search,
-            color: _page == 1 ? Colors.black54 : secondaryColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? Colors.black54 : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
           ),
-          label: "",
-          backgroundColor: primaryColor,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.add_circle,
-            color: _page == 2 ? Colors.black54 : secondaryColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? Colors.black54 : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
           ),
-          label: "",
-          backgroundColor: primaryColor,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.favorite,
-            color: _page == 3 ? Colors.black54 : secondaryColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? Colors.black54 : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
           ),
-          label: "",
-          backgroundColor: primaryColor,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.person,
-            color: _page == 4 ? Colors.black54 : secondaryColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? Colors.black54 : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
           ),
-          label: "",
-          backgroundColor: primaryColor,
-        ),
-      ]),
+        ],
+        onTap: navigationTapped,
+      ),
     );
   }
 }
