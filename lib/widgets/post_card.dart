c817ed9be77df504da19bec81aab16638dfaf996
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-05 00:59:34 +0100

    working on post_card

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+    );
+  }
+}
